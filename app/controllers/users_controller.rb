12a093a6d1cf575244ab6a50ce22d6bec261c860
徳永 優作 <yusaku_tokunaga@r.recruit.co.jp>
2015-09-12 23:48:29 +0900

    POSTされたやつを登録できるようにした

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,19 @@
 class UsersController < ApplicationController
   def index
-    tmp = {'name' => 'Yamada to Takashi', 'old' => 28}
+    tmp = {'name' => 'Yamada!!!!!!!!!!!', 'old' => 28}
     render :json => tmp
   end
 
   def show
   end
+
+  def create
+    @newUser = User.create(user_params)
+      render :json => @newUser, status: :created # JSON形式
+  end
+
+  def user_params
+    params.permit(:name, :pass, :gcm_id)
+  end
+
 end
